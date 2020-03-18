9eb29ecc5c0d5030e5865701ade749296b797733
UmutKorkmaz <umutkorkmaz@outlook.com.tr>
2020-03-18 04:22:20 +0300

    Fetching a json file to populate the monsters array

--- src/App.js
+++ src/App.js
@@ -7,22 +7,16 @@ class App extends React.Component{
     super();
 
     this.state = {
-      monsters:[
-        {
-          name:'Frankenstein',
-          id: '1'
-        },
-        {
-          name:'Dracula',
-          id: '2'
-        },
-        {
-          name:'Zombie',
-          id: '3'
-        }
-      ]
+      monsters:[]
     }
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}));
+  }
+  
   render(){
     return(
       <div className="App">
