8eb828d74c68c0ba817bd963c95498532eba0762
UmutKorkmaz <umutkorkmaz@outlook.com.tr>
2020-03-22 02:32:16 +0300

    Updates

---
+++ precache-manifest.2450560192e4f85e1872f898953ebb98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64a23c862370bdd1bfe4fa32759741b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a0c944209d6b53c5b0f",
+    "url": "/monsters-rolodex/static/css/main.a3b5dd64.chunk.css"
+  },
+  {
+    "revision": "eb8c292b02fc765d0844",
+    "url": "/monsters-rolodex/static/js/2.dca1f432.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.dca1f432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0c944209d6b53c5b0f",
+    "url": "/monsters-rolodex/static/js/main.3133c568.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
